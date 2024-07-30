3147afbd39e7fdaea53e96ff44aea20aee5d8682
Xyasskangboyah <161273548+Xyasskangboyah@users.noreply.github.com>
2024-07-30 11:25:45 +0700

    Update jvascript.js

--- javaScript/jvascript.js
+++ javaScript/jvascript.js
@@ -1,1 +1,17 @@
-document.write("hello world");
+// document.write("hello world");
+console.log("ilyas")
+
+// no 1
+var a;
+// no 2
+var a = null
+console.log(a);
+// no 3
+const name = "ilyas"
+var lastname = "khairul"
+var umur = "17 tahun"
+console.log(name +' '+ lastname +" " + umur);
+// no 4
+var alas = 5
+var tinggi = 7
+console.log(1/2 * alas * tinggi);
