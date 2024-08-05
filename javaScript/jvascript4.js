10e05ee285d772c14a9eeab19d8277506e7d5c4c
Xyasskangboyah <161273548+Xyasskangboyah@users.noreply.github.com>
2024-08-05 13:40:37 +0700

    jv script4

---
+++ javaScript/jvascript4.js
@@ -0,0 +1,43 @@
+var nilaiUjian = 92;
+var Kehadiran = 10;
+var TotalPertemuan = 25;
+var lulus = (Kehadiran / TotalPertemuan) * 100;
+
+if (nilaiUjian > 85) {
+  console.log("Kategori A");
+  if (lulus >= 75) {
+    console.log("anda lulus!");
+  } else {
+    console.log("anda gagal!");
+  }
+} else if (nilaiUjian > 70 && nilaiUjian <= 85) {
+  console.log("Kategori B");
+  if (lulus >= 75) {
+    console.log("anda lulus!");
+  } else {
+    console.log("anda gagal!");
+  }
+} else if (nilaiUjian > 50 && nilaiUjian <= 70) {
+  console.log("kategori C");
+  if (lulus >= 75) {
+    console.log("anda lulus");
+  } else {
+    console.log("anda gagal!");
+  }
+} else if (nilaiUjian < 50) {
+  console.log("Tidak lulus");
+}
+
+
+var pendaftaran = false;
+var pembayaran = false;
+
+if (pendaftaran === true) {
+  if (pembayaran === true) {
+    console.log("Dapat mengikuti Kursus");
+  } else {
+    console.log("Tidak dapat mengikuti Kursus");
+  }
+} else {
+  console.log("Tidak dapat mengikuti Kursus");
+}
